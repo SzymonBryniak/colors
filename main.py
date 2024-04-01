f37e84af72e63338c206615594c2600034555a3f
SzymonBryniak <szymonbryniakproject@gmail.com>
2024-04-01 19:22:37 +0100

    final painting

--- main.py
+++ main.py
@@ -10,3 +10,22 @@ for i in range(len(colors)):
     colorsExtracted.append(Append)
 #com
 print(colorsExtracted)
+
+import turtle as t
+import random as rn
+                        #TODO:20 dots spaced by 50
+screen = t.Screen()
+screen.colormode(255)
+colorList = [(245, 243, 238), (246, 242, 244), (202, 164, 110), (240, 245, 241), (236, 239, 243), (149, 75, 50), (222, 201, 136), (53, 93, 123), (170, 154, 41), (138, 31, 20)]
+
+
+t.penup()
+for i in range(10):
+    t.setpos(-200, 50*i)
+    for c in range(10):
+        ranColor = rn.choice(colorList)
+        t.forward(50)
+        t.dot(20, ranColor)
+
+screen.exitonclick()
+
